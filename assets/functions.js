dbd942f1384c6748b6031315273cd604618ae31f
omwitsa-etole <omwitsaetole@gmail.com>
2024-01-21 17:48:23 +0300

    1.1

---
+++ assets/functions.js
@@ -0,0 +1,299 @@
+let EL = document.getElementById("pageRoot");
+const body = EL;
+
+const layouts = ["Parallel","Angled","Single Level Lots","Multi-Level Lots"];
+const rates = ["hourly","flat"]
+
+
+const startFunction =(e)=>{
+	EL = document.getElementById("pageRoot");
+	
+}
+window.addEventListener("load",startFunction);
+
+function addParking(){
+	let el = document.createElement("div");
+	el.classList.add("normal")
+	el.setAttribute("style","width: 620px;")
+	let types = ""
+	
+	let rateType = ""
+	for(const layout of layouts.keys()){
+		console.log(layouts[layout])
+		types +=`<option value=${layouts[layout]}>${layouts[layout]}</option>`; 
+	} 
+	for(const rate of rates){
+		rateType += `<option value=${rate}>${rate}</option>`
+	}
+	el.innerHTML = `
+<div class="page-content" style="width: 600px;">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-10 col-xl-9" style="width: 100%;height: 100%;position:relative;">
+        <div class="card-new" style="width: 100%;">
+          <div class="form-step form-step1">
+            <div class="form-step-head card-innr">
+              <div class="step-head">
+                <div class="step-number">01</div>
+                <div class="step-head-text">
+                  <h4>Parking Details</h4>
+                  
+                </div>
+              </div>
+            </div>
+           
+            <div class="form-step-fields card-innr">
+              <div class="note note-plane note-light-alt note-md pdb-1x">
+                
+                
+              </div>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="input-item input-with-label">
+                    <label class="input-item-label">
+                      Name <span class="text-danger">*</span>
+                    </label>
+                    <input class="input-bordered" type="text" id="name" />
+                  </div>
+                 
+                </div>
+              
+               
+              
+                <div class="col-md-6">
+                  <div class="input-item input-with-label">
+                    <label class="input-item-label">
+                      Address <span class="text-danger">*</span>
+                    </label>
+                    <input class="input-bordered" type="text" id="address" />
+                  </div>
+                 
+                </div>
+               
+                <div class="col-md-6">
+                  <div class="input-item input-with-label">
+                    <label class="input-item-label">
+                     Slots <span class="text-danger">*</span>
+                    </label>
+                    <input class="input-bordered" type="number" id="slots"/>
+                  </div>
+                
+                </div>
+               
+                
+				
+				<div class="col-md-6">
+                  <div class="input-item input-with-label">
+                    <label class="input-item-label">
+                      Type <span class="text-danger">*</span>
+                    </label>
+                    <select>
+						${types}
+					</select>
+                  </div>
+                  
+                </div>
+				
+				<div class="col-md-6">
+                  <div class="input-item input-with-label">
+                    <label class="input-item-label">
+                      Rate <span class="text-danger">*</span>
+                    </label>
+                    <input class="input-bordered date-picker" type="number" id="rate-value" />
+                  </div>
+                  
+                </div>
+				
+				<div class="col-md-6">
+                  <div class="input-item input-with-label">
+                    <label class="input-item-label">
+                      Rate type <span class="text-danger">*</span>
+                    </label>
+                    <select id="rate-type">${rateType}</select>
+                  </div>
+                  
+                </div>
+              
+              </div>
+              <div class="btn-card">
+              <button class="btn-submit" onclick="newParking()">Submit</button>
+			  <button class="btn-submit cancel" onclick="closeTab()">Cancel</button>
+			  </div>
+            
+          </div>
+        </div>
+       
+      </div>
+    </div>
+  </div>
+  
+</div>
+	
+	`;
+	body.innerHTML = "";
+	
+	body.appendChild(el);
+	body.setAttribute("style","display:block;")
+	
+}
+
+function newParking(){
+	
+	let el = document.createElement("div");
+	el.classList.add("normal");
+	el.innerHTML = `
+
+<div class="card"> 
+  <button type="button" class="dismiss" onclick="addParking()">×</button> 
+  <div class="header"> 
+    <div class="image">
+      
+	  </div> 
+      <div class="content">
+         <span class="title">Parking space added</span> 
+         <p class="message">Your parking space has been added, continue to add slots in preview</p> 
+         </div> 
+         <div class="actions">
+            <button type="button" class="history" onclick="previewParking()">Preview</button> 
+            <button type="button" class="track" onclick="addParking()">Back</button> 
+</div> 
+</div> 
+</div>
+
+	`;
+	openTab();
+	body.appendChild(el);
+}
+
+function previewParking(){
+	let el = document.createElement("div");
+	el.classList.add("normal");
+	el.innerHTML = `Preview parking spots`;
+	openTab();
+	body.appendChild(el);
+}
+
+function closeTab(){
+	body.removeAttribute("style");
+}
+function openTab(){
+	body.setAttribute("style","display:block");
+}
+
+function parkingMap(){
+	const body = document.getElementById("page-content");
+	body.innerHTML = `
+	<div style="position:relative;width: 100%;">
+	<h4>ParkingMap</h4>
+	<a class="settings-map" id="settings-map">
+		<i class="fa-solid fa-bars"></i>
+	</a>
+	<hr/>
+	</div>
+	<div class="map-container">
+		<div class="parkingmap map-row">
+		
+			<div class="parking-header">
+				<h4>Parking Space Map</h4>
+				<a class="refresh-map" onclick="loadMap()">
+					<i class="fa-solid fa-rotate"></i>
+				</a>
+			</div>
+			<div class="parking-body" id="container">
+				
+			</div>
+		</div>
+		<div class="map-settings map-row" id="map-settings">
+			<div class="parking-header">
+				<h4>Settings</h4>
+				
+			</div>
+			<div class="settings-body" id="content">
+				<ul class="setting-options">
+					<li value="path" onclick="newContext(this.value)">Add Path </li>
+					<li value="slot" onclick="newContext(this.value)">Add Slot </li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	`;
+	$("#settings-map").click(()=>{
+		const el = document.getElementById("map-settings");
+		if(el.classList.contains("showing")){
+			el.classList.remove("showing");
+		}else{
+			el.classList.add("showing");
+		}
+	})
+	loadMap()
+}
+
+
+function loadMap(){
+	const container = document.getElementById("container")
+	const map = document.createElement("div")
+	map.setAttribute("class","radio-tile-group")
+	let spot = "";
+	const bg = ["none","#ff7474"];
+	const stet = ["Parked","Free"];
+	for(let i=0;i<18;i++){
+		let x = Math.floor(Math.random() * 2);
+		spot += `
+			<div class="input-container">
+			  <input id=${"drive"+i} class="radio-button" type="radio" name="radio">
+			  <div class="radio-tile">
+				<div class="icon car-icon">
+				  <svg fill=${bg[x]} height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+					<path d="M18.92 6.01C18.72 5.42 18.16 5 17.5 5h-11c-.66 0-1.21.42-1.42 1.01L3 12v8c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-1h12v1c0 .55.45 1 1 1h1c.55 0 1-.45 1-1v-8l-2.08-5.99zM6.5 16c-.83 0-1.5-.67-1.5-1.5S5.67 13 6.5 13s1.5.67 1.5 1.5S7.33 16 6.5 16zm11 0c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zM5 11l1.5-4.5h11L19 11H5z"></path>
+					<path d="M0 0h24v24H0z" fill=${bg[x]}></path>
+				  </svg>
+				</div>
+				<label for=${"drive"+i} class="radio-tile-label">${stet[x]}</label>
+			  </div>
+			</div>
+		`;
+	}
+	map.innerHTML = spot
+	
+	if(container.children[0]){
+		container.children[0].remove()
+		
+	}
+	container.appendChild(map)
+	
+	$(document).on('click', '.radio-button', function(e) {
+		
+		showContent(e.target)
+	});
+	container.setAttribute("style","width:99%;height:586px;box-shadow: rgba(3, 102, 214, 0.3) 0px 0px 0px 3px;")
+}
+
+function showContent(el){
+	const content = document.getElementById("content")
+	content.innerHTML = `
+		<div style="width:100%;">
+			  <div class="p-3" style="background-color: #eee;">
+				<span class="fw-bold">Order Recap</span>
+				 <div class="d-flex justify-content-between mt-2">
+				  <span>Booking ID </span> <span>${el.id}</span>
+				</div>
+				<div class="d-flex justify-content-between mt-2">
+				  <span class="lh-sm">Text here   </span>
+				  <span>$40.00</span>
+				</div>
+				<hr />
+				<div class="d-flex justify-content-between mt-2">
+				  <span>Text here </span> <span>$71.76</span>
+				</div>
+				<div class="d-flex justify-content-between mt-2">
+				  <span>Text here </span> <span>$71.76</span>
+				</div>
+				<hr />
+				<div class="d-flex justify-content-between mt-2">
+				  <span>Text here </span> <span class="text-success">$85.00</span>
+				</div>
+			  </div>
+			</div>
+	`;
+	
+}
--- js/settings.js
+++ js/settings.js
@@ -1,208 +1,337 @@
 function rateSettings(){
+	const rates = ["hourly","flat","extension"]
+	const availableRates = ()=>{
+		let row = "";
+		for(let i = 1;i<=rates.length;i++){
+			let switchid = "switch_tds"+i
+			let id = ""+i
+			row += "<div class='flexrow'>"
+			row += `
+				<div class="container py-5">
+					<div class="row d-flex justify-content-center">
+					  <div class="col-md-8 col-lg-6 col-xl-4" style="width:90%;">
+						<div class="card rounded-3">
+						  <div class="card-body mx-1 my-2">
+							<div class="onoffswitch">
+								<input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id=${switchid}>
+								<label class="onoffswitch-label" for=${switchid}>
+								<span class="onoffswitch-inner"></span>
+								<span class="onoffswitch-switch"></span>
+								</label>
+							</div>
+							<div class="d-flex align-items-center">
+							  <div>
+								<i class=""></i>
+							  </div>
+							  <div>
+								<p class="d-flex flex-column mb-0">
+								  <b>${rates[i-1].toUpperCase()}</b><span class="small text-muted">rate</span>
+								</p>
+							  </div>
+							</div>
+
+							<div class="pt-3">
+							  <div class="d-flex flex-row pb-3">
+								<div class="rounded border border-primary border-2 d-flex w-100 p-3 align-items-center"
+								  style="background-color: rgba(18, 101, 241, 0.07);">
+								  <div class="d-flex align-items-center pe-3">
+									<input class="form-check-input" type="radio" name=${"radioNoLabelX"+id} id=${"radioNoLabel"+id}
+									  value="" aria-label="..." checked />
+								  </div>
+								  <div class="d-flex flex-column">
+									<p class="mb-1 small text-primary">Total amount paid</p>
+									<h6 class="mb-0 text-primary">$8245</h6>
+								  </div>
+								</div>
+							  </div>
+
+							  <div class="d-flex flex-row pb-3">
+								<div class="rounded border border-primary border-2 d-flex w-100 p-3 align-items-center"
+								  style="background-color: rgba(18, 101, 241, 0.07);">
+								  <div class="d-flex align-items-center pe-3">
+									<input class="form-check-input" type="radio" name=${"radioNoLabelY"+id} id=${"radioNoLabel"+id}
+									  value="" aria-label="..." checked />
+								  </div>
+								  <div class="d-flex flex-column">
+									<p class="mb-1 small text-primary">Rate Value</p>
+									<h6 class="mb-0 text-primary">$</h6>
+									<input type="text" class="form-control form-control-sm" id="numberExample"
+										style="width: 55px;" />
+								  </div>
+								</div>
+							  </div>
+							</div>
+
+							<div class="d-flex justify-content-between align-items-center pb-1">
+							  <a href="javascript:void(0)" class="text-muted">Delete</a>
+							  <button type="button" class="btn btn-primary btn-lg">Save</button>
+							</div>
+						  </div>
+						</div>
+					  </div>
+					</div>
+				  </div>
+			`;	
+			row+="</div>"
+		 }
+		return row;
+	}
 	const body = document.getElementById("page-content");
 	body.innerHTML = `
-<div class="content container-fluid" style="margin-top:5%;margin-left:5%;">
-<div class="row">
-<div class="col-md-8 offset-md-2">
+		<div class="content container-fluid" style="margin-top:9%;margin-left:5%;">
+		<div class="row">
+			<div class="h3 card-title with-switch" style="width:89%;margin-left:15%;">
+			Rate Settings&nbsp; <small class="form-text text-muted">Availabe</small>
+			<div class="form-group" style="position:absolute;right:5%;margin-top:-45px;">
+			<input class="addnew addsave" type="button" value="Save" />
+			<input class="addnew" type="button" value="Add New" id="addnew"/>
 
-<div class="page-header">
-<div class="row">
-<div class="col-sm-12">
-<h3 class="page-title">Rate Settings</h3>
-</div>
-</div>
-</div>
-
-<form>
-
-<div class="settings-widget">
-<div class="h3 card-title with-switch">
-Hourly Rate 
-<div class="onoffswitch">
-<input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="switch_hra" checked="">
-<label class="onoffswitch-label" for="switch_hra">
-<span class="onoffswitch-inner"></span>
-<span class="onoffswitch-switch"></span>
-</label>
-</div>
-</div>
-<div class="row">
-<div class="col-sm-6">
-<div class="form-group">
-<label>Current (KES)</label>
-<input type="text" class="form-control">
-</div>
-</div>
-<div class="col-sm-6">
-<div class="form-group">
-<label>NEW (KES)</label>
-<input class="form-control" type="text">
-</div>
-</div>
-</div>
-</div>
-
-
-<div class="settings-widget">
-<div class="h3 card-title with-switch">
-Flat Rate
-<div class="onoffswitch">
-<input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="switch_pf" checked="">
-<label class="onoffswitch-label" for="switch_pf">
-<span class="onoffswitch-inner"></span>
-<span class="onoffswitch-switch"></span>
-</label>
-</div>
-</div>
-<div class="row">
-<div class="col-sm-6">
-<div class="form-group">
-<label>Current (KES)</label>
-<input class="form-control" type="text">
-</div>
-</div>
-<div class="col-sm-6">
-<div class="form-group">
-<label>NEW (KES)</label>
-<input class="form-control" type="text">
-</div>
-</div>
-</div>
-</div>
-
-
-<div class="settings-widget">
-<div class="h3 card-title with-switch">
-Extended Time
-<div class="onoffswitch">
-<input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="switch_esi">
-<label class="onoffswitch-label" for="switch_esi">
-<span class="onoffswitch-inner"></span>
-<span class="onoffswitch-switch"></span>
-</label>
-</div>
-</div>
-<div class="row">
-<div class="col-sm-6">
-<div class="form-group">
-<label>Current (KES)</label>
-<input class="form-control" type="text">
-</div>
-</div>
-<div class="col-sm-6">
-<div class="form-group">
-<label>New (KES)</label>
-<input class="form-control" type="text">
-</div>
-</div>
-</div>
-</div>
-
-<div class="submit-section">
-<button class="btn btn-primary submit-btn" type="submit">Save</button>
-</div>
-
-</form>
-</div>
-</div>
-</div>
+			</div>
+			</div>
+			<div class="flexdisplay textleft" id="availablepayment">
+			${availableRates()}
+			</div>
+			
+		</div>
 	`;
+	$("#addnew").click(()=>{
+		let id = Math.floor(Math.random() * 999);
+		id = ""+id
+		const el = document.createElement("div")
+		el.setAttribute("class","floating")
+		el.setAttribute("id",id)
+		el.innerHTML = `
+			<div class="" style="width:100%;margin-left:0;" style="position:relative;">
+
+				<div class="flex" id="paymenttab">
+
+				<div class="row row-sm">
+					
+					<div class="col-sm-4">
+						<div class="form-group">
+						 <label>Name *</label>
+						<input class="form-control" type="text">
+						</div>
+					</div>
+				<div class="col-sm-4">
+				<div class="form-group">
+				<label>Account *</label>
+				<input class="form-control" type="text">
+				</div>
+				</div>
+				<div class="col-sm-2">
+				<div class="form-group">
+				<label>Paybill</label>
+				<input class="form-control" type="text">
+				</div>
+				</div>
+				<div class="col-sm-2">
+
+				<label class="d-none d-sm-block">&nbsp;</label>
+				<button class="btn btn-danger w-100 set-btn form-control" type="button" onclick="deletePayment(this.parentElement.parentElement)"><i class="fa fa-trash-o"></i></button>
+
+				</div>
+				</div>
+
+				</div>
+				<div class="row row-sm">
+					<div class="col-sm-2 ms-auto">
+						<div class="form-group">
+							<button class="btn btn-primary w-100 form-control" type="button" id="addpayment"><i class="fa fa-plus"></i></button>
+						</div>
+					</div>
+				</div>
+
+				<div class="" style="position:absolute;bottom:4%;left:0;width:100%;display:flex;align-items:center;z-index:2;">
+							<button class="btn btn-primary w-100 form-control" type="button" onclick="deletePayment(document.getElementById(${id}))" style="background:#23232b;">Cancel</button>
+							<button class="btn btn-primary w-100 form-control" type="button" style="float:right;"><i class="fa fa-file"></i> Save</button>						
+				</div>
+			</div>
+		`;
+		body.appendChild(el)
+	})
 }
 
 function paymentPage(){
 	const body = document.getElementById("page-content")
+	const availablePayment = ()=>{
+		let row = "";
+		for(let i = 1;i<5;i++){
+			let switchid = "switch_tds"+i
+			let id = ""+i
+			row += "<div class='flexrow'>"
+			row += `
+				<div class="container py-5">
+					<div class="row d-flex justify-content-center">
+					  <div class="col-md-8 col-lg-6 col-xl-4" style="width:90%;">
+						<div class="card rounded-3">
+						  <div class="card-body mx-1 my-2">
+							<div class="onoffswitch">
+								<input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id=${switchid}>
+								<label class="onoffswitch-label" for=${switchid}>
+								<span class="onoffswitch-inner"></span>
+								<span class="onoffswitch-switch"></span>
+								</label>
+							</div>
+							<div class="d-flex align-items-center">
+							  <div>
+								<i class="fab fa-cc-visa fa-4x text-black pe-3"></i>
+							  </div>
+							  <div>
+								<p class="d-flex flex-column mb-0">
+								  <b>Martina Thomas</b><span class="small text-muted">**** 8880</span>
+								</p>
+							  </div>
+							</div>
+
+							<div class="pt-3">
+							  <div class="d-flex flex-row pb-3">
+								<div class="rounded border border-primary border-2 d-flex w-100 p-3 align-items-center"
+								  style="background-color: rgba(18, 101, 241, 0.07);">
+								  <div class="d-flex align-items-center pe-3">
+									<input class="form-check-input" type="radio" name=${"radioNoLabelX"+id} id=${"radioNoLabel"+id}
+									  value="" aria-label="..." checked />
+								  </div>
+								  <div class="d-flex flex-column">
+									<p class="mb-1 small text-primary">Total amount paid</p>
+									<h6 class="mb-0 text-primary">$8245</h6>
+								  </div>
+								</div>
+							  </div>
+
+							  <div class="d-flex flex-row pb-3">
+								<div class="rounded border border-primary border-2 d-flex w-100 p-3 align-items-center"
+								  style="background-color: rgba(18, 101, 241, 0.07);">
+								  <div class="d-flex align-items-center pe-3">
+									<input class="form-check-input" type="radio" name=${"radioNoLabelY"+id} id=${"radioNoLabel"+id}
+									  value="" aria-label="..." checked />
+								  </div>
+								  <div class="d-flex flex-column">
+									<p class="mb-1 small text-primary">Available Funds</p>
+									<h6 class="mb-0 text-primary">$8245</h6>
+								  </div>
+								</div>
+							  </div>
+							</div>
+
+							<div class="d-flex justify-content-between align-items-center pb-1">
+							  <a href="javascript:void(0)" class="text-muted">Delete</a>
+							  <button type="button" class="btn btn-primary btn-lg">Withdraw</button>
+							</div>
+						  </div>
+						</div>
+					  </div>
+					</div>
+				  </div>
+			`;	
+			row+="</div>"
+		 }
+		return row;
+	}
 	body.innerHTML = `
 <div class="content container-fluid" style="margin-top:11%;margin-left:5%;">
 <div class="row">
-<div class="col-md-8 offset-md-2">
-
-<div class="settings-widget">
-<div class="h3 card-title with-switch">
+<div class="h3 card-title with-switch" style="width:89%;margin-left:15%;">
 Payment Modes&nbsp; <small class="form-text text-muted">Availabe</small>
-<div class="onoffswitch">
-<input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="switch_tds">
-<label class="onoffswitch-label" for="switch_tds">
-<span class="onoffswitch-inner"></span>
-<span class="onoffswitch-switch"></span>
-</label>
-</div>
-</div>
-
-<div class="flexdisplay" id="paymenttab">
-
-<div class="row row-sm">
-<div class="col-sm-4">
-<div class="form-group">
- <label>Name *</label>
-<input class="form-control" type="text">
-</div>
-</div>
-<div class="col-sm-4">
-<div class="form-group">
-<label>Account *</label>
-<input class="form-control" type="text">
-</div>
-</div>
-<div class="col-sm-2">
-<div class="form-group">
-<label>Paybill</label>
-<input class="form-control" type="text">
-</div>
-</div>
-<div class="col-sm-2">
-
-<label class="d-none d-sm-block">&nbsp;</label>
-<button class="btn btn-danger w-100 set-btn form-control" type="button" onclick="deletePayment(this.parentElement.parentElement)"><i class="fa fa-trash-o"></i></button>
+<div class="form-group" style="position:absolute;right:5%;margin-top:-45px;">
+<input class="addnew addsave" type="button" value="Save" />
+<input class="addnew" type="button" value="Add New" id="addnew"/>
 
 </div>
 </div>
-
-</div>
-
-<div class="row row-sm">
-<div class="col-sm-2 ms-auto">
-<div class="form-group">
-<button class="btn btn-primary w-100 form-control" type="button" id="addpayment"><i class="fa fa-plus"></i></button>
-</div>
-</div>
+<div class="flexdisplay textleft" id="availablepayment">
+${availablePayment()}
 </div>
 </div>
 
-</div>
-</div>
+
 </div>
 	`;
-	
-	$("#addpayment").click(()=>{
-		document.getElementById("paymenttab").innerHTML += `
-			<div class="row row-sm" >
-			<div class="col-sm-4">
-			<div class="form-group">
-			 <label>Name *</label>
-			<input class="form-control" type="text">
-			</div>
-			</div>
-			<div class="col-sm-4">
-			<div class="form-group">
-			<label>Account *</label>
-			<input class="form-control" type="text">
-			</div>
-			</div>
-			<div class="col-sm-2">
-			<div class="form-group">
-			<label>Paybill </label>
-			<input class="form-control" type="text">
-			</div>
-			</div>
-			<div class="col-sm-2">
-			
-			<label class="d-none d-sm-block">&nbsp;</label>
-			<button class="btn btn-danger w-100 set-btn form-control" type="button" onclick="deletePayment(this.parentElement.parentElement)"><i class="fa fa-trash-o"></i></button>
-			
-			</div>
+	$("#addnew").click(()=>{
+		let id = Math.floor(Math.random() * 999);
+		id = ""+id
+		const el = document.createElement("div")
+		el.setAttribute("class","floating")
+		el.setAttribute("id",id)
+		el.innerHTML = `
+			<div class="" style="width:100%;margin-left:0;" style="position:relative;">
+
+				<div class="flex" id="paymenttab">
+
+				<div class="row row-sm">
+					
+					<div class="col-sm-4">
+						<div class="form-group">
+						 <label>Name *</label>
+						<input class="form-control" type="text">
+						</div>
+					</div>
+				<div class="col-sm-4">
+				<div class="form-group">
+				<label>Account *</label>
+				<input class="form-control" type="text">
+				</div>
+				</div>
+				<div class="col-sm-2">
+				<div class="form-group">
+				<label>Paybill</label>
+				<input class="form-control" type="text">
+				</div>
+				</div>
+				<div class="col-sm-2">
+
+				<label class="d-none d-sm-block">&nbsp;</label>
+				<button class="btn btn-danger w-100 set-btn form-control" type="button" onclick="deletePayment(this.parentElement.parentElement)"><i class="fa fa-trash-o"></i></button>
+
+				</div>
+				</div>
+
+				</div>
+				<div class="row row-sm">
+					<div class="col-sm-2 ms-auto">
+						<div class="form-group">
+							<button class="btn btn-primary w-100 form-control" type="button" id="addpayment"><i class="fa fa-plus"></i></button>
+						</div>
+					</div>
+				</div>
+
+				<div class="" style="position:absolute;bottom:4%;left:0;width:100%;display:flex;align-items:center;z-index:2;">
+							<button class="btn btn-primary w-100 form-control" type="button" onclick="deletePayment(document.getElementById(${id}))" style="background:#23232b;">Cancel</button>
+							<button class="btn btn-primary w-100 form-control" type="button" style="float:right;"><i class="fa fa-file"></i> Save</button>						
+				</div>
 			</div>
 		`;
+		body.appendChild(el)
+		$("#addpayment").click(()=>{
+			document.getElementById("paymenttab").innerHTML += `
+				<div class="row row-sm" >
+				<div class="col-sm-4">
+				<div class="form-group">
+				 <label>Name *</label>
+				<input class="form-control" type="text">
+				</div>
+				</div>
+				<div class="col-sm-4">
+				<div class="form-group">
+				<label>Account *</label>
+				<input class="form-control" type="text">
+				</div>
+				</div>
+				<div class="col-sm-2">
+				<div class="form-group">
+				<label>Paybill </label>
+				<input class="form-control" type="text">
+				</div>
+				</div>
+				<div class="col-sm-2">
+				<label class="d-none d-sm-block">&nbsp;</label>
+				<button class="btn btn-danger w-100 set-btn form-control" type="button" onclick="deletePayment(this.parentElement.parentElement)"><i class="fa fa-trash-o"></i></button>
+				</div>
+				</div>
+			`;
+		})
 	})
+	
 }
 function deletePayment(el){
 	el.remove()
